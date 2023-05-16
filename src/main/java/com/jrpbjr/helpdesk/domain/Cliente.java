042c7d7fe723bc0825e4dca3decd6013e68214ee
Jrpbjr <jrpbjr@gmail.com>
2023-05-16 10:56:29 -0300

    Protegendo aplicação contra serializacao

--- src/main/java/com/jrpbjr/helpdesk/domain/Cliente.java
+++ src/main/java/com/jrpbjr/helpdesk/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jrpbjr.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -16,6 +17,7 @@ public class Cliente extends Pessoa{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
