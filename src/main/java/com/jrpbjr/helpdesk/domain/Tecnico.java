d14927982a456a07f6d7b6a9d404690a5ab49cee
Jrpbjr <jrpbjr@gmail.com>
2023-05-15 21:39:59 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/jrpbjr/helpdesk/domain/Tecnico.java
+++ src/main/java/com/jrpbjr/helpdesk/domain/Tecnico.java
@@ -3,18 +3,32 @@ package com.jrpbjr.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
-	
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jrpbjr.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
 		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
@@ -24,7 +38,5 @@ public class Tecnico extends Pessoa{
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-	
-	
 
 }
