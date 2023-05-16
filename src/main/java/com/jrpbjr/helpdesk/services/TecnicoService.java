394f38bc0f5bf50caf5bd61aab22fe058a7cf16e
Jrpbjr <jrpbjr@gmail.com>
2023-05-16 18:00:13 -0300

    Exceção para objeto nao encontrado

--- src/main/java/com/jrpbjr/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jrpbjr/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jrpbjr.helpdesk.domain.Tecnico;
 import com.jrpbjr.helpdesk.repositories.TecnicoRepository;
+import com.jrpbjr.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id:" + id));
 	}
 
 }
---
+++ src/main/java/com/jrpbjr/helpdesk/services/exceptions/ObjectnotFoundException.java
@@ -0,0 +1,22 @@
+package com.jrpbjr.helpdesk.services.exceptions;
+
+public class ObjectnotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ObjectnotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectnotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
