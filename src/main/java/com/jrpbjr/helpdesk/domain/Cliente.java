cf418ba7b64c13bf65edf11f7cbb9054b615e9a7
Jrpbjr <jrpbjr@gmail.com>
2023-05-18 18:20:20 -0300

    CRUD para Cliente implementado

--- src/main/java/com/jrpbjr/helpdesk/domain/Cliente.java
+++ src/main/java/com/jrpbjr/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.jrpbjr.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jrpbjr.helpdesk.domain.dtos.ClienteDTO;
 import com.jrpbjr.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -33,6 +35,19 @@ public class Cliente extends Pessoa{
 		// TODO Auto-generated constructor stub
 		addPerfil(Perfil.CLIENTE);
 	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfil = obj.getPerfil().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+		addPerfil(Perfil.CLIENTE);
+	}
+
 
 	public List<Chamado> getChamados() {
 		return chamados;
