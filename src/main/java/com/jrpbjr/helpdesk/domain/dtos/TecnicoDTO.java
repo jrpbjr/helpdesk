e303db8b3cab820ed100295c85f5d5f3efa8c533
Jrpbjr <jrpbjr@gmail.com>
2023-05-17 22:16:01 -0300

    Validacao nos campos de criaccao de novo usuario

--- src/main/java/com/jrpbjr/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/jrpbjr/helpdesk/domain/dtos/TecnicoDTO.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.jrpbjr.helpdesk.domain.Tecnico;
 import com.jrpbjr.helpdesk.domain.enums.Perfil;
 
@@ -18,9 +20,13 @@ public class TecnicoDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "O campo EMAIL é requerido")
 	protected String email;
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfil = new HashSet<>();
 	
